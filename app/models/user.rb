78eeca41dc971af0a96617e487844d113c9ba5fa
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-23 12:25:42 -0500

    Refactor user model validations: add uniqueness and length constraints for username and email, and include email format validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # prevents user creation with null input
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 255 }, format: {
+    with: URI::MailTo::EMAIL_REGEXP
+  }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
