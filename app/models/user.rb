7b29e2d434cf31f727719363382f82f539c8b5f5
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-23 12:14:48 -0500

    Refactor user model and migration: add presence validations for username, email, first name, and last name; update schema and fixtures accordingly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # prevents user creation with null input
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_233510) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_23_165439) do
+  create_table "portfolio_stocks", force: :cascade do |t|
+    t.integer "portfolio_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_portfolio_stocks_on_portfolio_id"
+    t.index ["stock_id"], name: "index_portfolio_stocks_on_stock_id"
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recently_vieweds", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "stock_id", null: false
@@ -30,13 +45,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_17_233510) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "portfolio_stocks", "portfolios"
+  add_foreign_key "portfolio_stocks", "stocks"
   add_foreign_key "recently_vieweds", "stocks"
   add_foreign_key "recently_vieweds", "users"
 end
