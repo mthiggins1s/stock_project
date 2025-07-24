747eac4dff646848c381339cea4fe4ea75ccb73b
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-24 18:42:18 -0500

    Remove posts_index from before_action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :update, :destroy, :posts_index ]
+  before_action :set_user, only: [ :show, :update, :destroy ]
 
   def index
     users = User.all
