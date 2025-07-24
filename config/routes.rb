3a7b06053a31081d5995072c6ff9ae1ee7671f6a
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-24 11:06:21 -0500

    creating show, create, update, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   resources :stocks, only: [ :index, :show ]
   # localhost:3000/users
   get "/users", to: "users#index"
+
+  # localhost:3000/users/1
+  get "/users/:id", to: "users#show"
+
+  # localhost:3000/users
+  post "/users", to: "users#create"
+
+  # localhost:3000/users/1 (1 can be anything; example it can be 5 4 3 2 1)
+  put "/users/:id", to: "users#update"
+
+  # localhost:3000/users/1
+  delete "/users/:id", to: "users#destroy"
 end
