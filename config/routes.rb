e916fe8f9197551df9a3279b48b307ec1b8999e1
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-25 10:54:06 -0500

    Add JWT authentication to sessions; implement login endpoint and related tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
+  get "sessions/create"
+  scope "/" do
+    post "login", to: "sessions#create"
+  end
   resources :stocks, only: [ :index, :show ]
-  # localhost:3000/users
+
+  # Users routes
   get "/users", to: "users#index"
-
-  # localhost:3000/users/1
   get "/users/:id", to: "users#show"
-
-  # localhost:3000/users
   post "/users", to: "users#create"
-
-  # localhost:3000/users/1 (1 can be anything; example it can be 5 4 3 2 1)
   put "/users/:id", to: "users#update"
-
-  # localhost:3000/users/1
   delete "/users/:id", to: "users#destroy"
 
   resources :users
