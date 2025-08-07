1218f14f02b12ca9225da4b1d45015b82c4e37b4
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-08-07 15:05:43 -0500

    fix: allow CORS for frontend.vercel.app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:4200"
+    origins "https://stock-project-frontend.vercel.app"
 
     resource "*",
       headers: :any,
