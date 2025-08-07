8ea78f1905ba98e442f7fd54e920cee8ea9f4e80
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-08-07 17:10:51 -0500

    fix: update CORS configuration to allow localhost for development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://stock-project-frontend.vercel.app"
+    origins "http://localhost:4200"
 
     resource "*",
       headers: :any,
